e0a3276fcfbe7ea4d0ef10f528eb120e15540b8e
fabreduarte <fabreduarte@gmail.com>
2023-10-18 14:17:45 -0300

    inicio

---
+++ beans-ioc-autowired/src/main/java/dio/beans/autowired/BeansIocAutowiredApplication.java
@@ -0,0 +1,13 @@
+package dio.beans.autowired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeansIocAutowiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeansIocAutowiredApplication.class, args);
+	}
+
+}
---
+++ beans-ioc-autowired/src/test/java/dio/beans/autowired/BeansIocAutowiredApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.beans.autowired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeansIocAutowiredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
